edb5a39221a76b8614e3a79242bcf70e2ef33765
Luke0515-luke <0515luke@gmail.com>
2026-08-05 20:54:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# 1. 先執行備份或下載資料
+python3 pull_backup.py
 
-python3 pull_backup.py &
+# 2. 切換到程式碼所在的資料夾
+cd /opt/render/project/src
 
-python3 app.py
-
+# 3. 啟動 Gunicorn 來跑你的 Flask 主程式
+gunicorn -w 2 -b 0.0.0.0:$PORT "app:create_app()"
